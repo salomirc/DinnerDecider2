11f80c0f3e8310471dd5683fef7f6dd915656037
salomirc <36732879+salomirc@users.noreply.github.com>
2019-11-15 12:25:56 +0200

    Added a job.

--- app/src/main/java/com/slx/dinnerdecider2/MainActivity.kt
+++ app/src/main/java/com/slx/dinnerdecider2/MainActivity.kt
@@ -28,6 +28,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var occupation = "AAA"
         //setContentView(R.layout.activity_main2)
 
         //SetConstraintLayoutHeight()
